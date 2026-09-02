ae3fd72cb541d2e4ddd5b5160d20f0b30a4b2b97
Nicollas <Pedronicollas2024@gmail.com>
2026-09-02 12:56:29 -0300

    Adiciona Parte 2 de Python: Tipos de Dados e Variaveis

---
+++ semestre-01/linguagem-programacao-python/parte02-tipos-dados-variaveis/README.md
@@ -0,0 +1,2 @@
+| `ex1-tipos-dados-resultado.png` | Print da execução no IDLE |
+
---
+++ semestre-01/linguagem-programacao-python/parte02-tipos-dados-variaveis/ex1-tipos-dados.py
@@ -0,0 +1,9 @@
+nome = "Pedro"        # string (texto)
+idade = 18             # int (inteiro)
+altura = 1.75          # float (decimal)
+estuda = True           # bool (verdadeiro/falso)
+
+print(nome)
+print(idade)
+print(altura)
+print(estuda)
