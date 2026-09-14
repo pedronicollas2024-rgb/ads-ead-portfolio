e7c41f6cb10c82f78cb8fd95f77d2ebc8690d808
Nicollas <Pedronicollas2024@gmail.com>
2026-09-13 23:20:42 -0300

    Adicionando imagens

---
+++ semestre-01/linguagem-programacao-python/parte04-operadores-relacionais-selecao/ex1-pode-dirigir.py
@@ -0,0 +1,10 @@
+Nota =  float(input("Digite a nota do aluno:"))
+
+if Nota >= 9:
+    print("Conceito A")
+elif Nota >= 7:
+    print ("Conceito B")
+elif Nota >= 5:
+    print ("Conceito C")
+else:
+    print ("Reprovado")
